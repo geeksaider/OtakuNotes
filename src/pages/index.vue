e0f686f55bee3a70f936486f93fc92c1ac100c27
geeksaider <nikita.bondarev.06@gmail.com>
2025-02-28 15:18:07 +0300

    add search timeout

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -9,41 +9,59 @@ const animeList = ref();
 const loading = ref<boolean>(false);
 const query = ref<string>("");
 
-watch(query, async () => {
-  if (!query.value) {
-    return;
-  }
-  loading.value = true;
-  try {
-    const response = await fetch(
-      `https://api.jikan.moe/v4/anime?q=${query.value}`
-    );
-    console.log(`https://api.jikan.moe/v4/anime?q=${query.value}`);
-    if (!response.ok) {
-      throw new Error("Ошибка сети");
+const fetchData = async (currentQuery: string) => {
+    if (!currentQuery) {
+        return;
     }
-    const json = await response.json();
-    animeList.value = json.data;
-  } catch (error) {
-    console.error("Ошибка при поиске:", error);
-  } finally {
-    loading.value = false;
-  }
+
+    loading.value = true;
+
+    try {
+        const response = await fetch(
+            `https://api.jikan.moe/v4/anime?q=${currentQuery}`
+        );
+        if (!response.ok) {
+            throw new Error("Ошибка сети");
+        }
+        const json = await response.json();
+        animeList.value = json.data;
+    } catch (error) {
+        console.warn("Ошибка при поиске:", error);
+    } finally {
+        loading.value = false;
+    }
+};
+
+let updateTimer: number = 0;
+
+watch(query, (value) => {
+    if (updateTimer) {
+        clearTimeout(updateTimer);
+    }
+    updateTimer = setTimeout(() => {
+        fetchData(value);
+    }, 500);
 });
 </script>
 
 <template>
-  <div class="min-h-[101vh] text-third flex flex-col">
-    <Header></Header>
-    <div class="bg-background flex-grow">
-      <div class="pt-12 max-w-[1200px] mx-auto flex flex-col gap-16">
-        <SearchInput class="justify-center" v-model="query"></SearchInput>
-        <MainField v-if="query == ''"></MainField>
-        <section class="grid grid-cols-4 gap-12" v-else>
-          <AnimeBanner v-for="anime in animeList" :selected-anime="anime" />
-        </section>
-      </div>
+    <div class="min-h-[101vh] text-third flex flex-col">
+        <Header></Header>
+        <div class="bg-background flex-grow">
+            <div class="pt-12 max-w-[1200px] mx-auto flex flex-col gap-16">
+                <SearchInput
+                    class="justify-center"
+                    v-model="query"
+                ></SearchInput>
+                <MainField v-if="query == ''"></MainField>
+                <section class="grid grid-cols-4 gap-12" v-else>
+                    <AnimeBanner
+                        v-for="anime in animeList"
+                        :selected-anime="anime"
+                    />
+                </section>
+            </div>
+        </div>
+        <Header></Header>
     </div>
-    <Header></Header>
-  </div>
 </template>
