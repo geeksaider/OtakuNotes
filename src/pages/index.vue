f90573022c4b941420ce575524ce102749a02046
geeksaider <nikita.bondarev.06@gmail.com>
2025-03-08 17:19:11 +0300

    loading animation

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -10,11 +10,7 @@ const loading = ref<boolean>(false);
 const query = ref<string>("");
 
 const fetchData = async (currentQuery: string) => {
-    if (!currentQuery) {
-        return;
-    }
-
-    loading.value = true;
+    if (!currentQuery) return;
 
     try {
         const response = await fetch(
@@ -35,33 +31,51 @@ const fetchData = async (currentQuery: string) => {
 let updateTimer: number = 0;
 
 watch(query, (value) => {
+    loading.value = true;
     if (updateTimer) {
         clearTimeout(updateTimer);
     }
     updateTimer = setTimeout(() => {
         fetchData(value);
-    }, 500);
+    }, 600);
 });
 </script>
 
 <template>
     <div class="min-h-[101vh] text-third flex flex-col">
-        <Header></Header>
-        <div class="bg-background flex-grow">
+        <Header />
+        <div class="bg-background flex-grow pb-20">
             <div class="pt-12 max-w-[1200px] mx-auto flex flex-col gap-16">
-                <SearchInput
-                    class="justify-center"
-                    v-model="query"
-                ></SearchInput>
-                <MainField v-if="query == ''"></MainField>
-                <section class="grid grid-cols-4 gap-12" v-else>
+                <SearchInput class="justify-center" v-model="query" />
+                <MainField v-if="query == ''" />
+
+                <section class="grid grid-cols-4 gap-12" v-if="!loading">
                     <AnimeBanner
                         v-for="anime in animeList"
                         :selected-anime="anime"
                     />
                 </section>
+                <section v-else class="flex justify-center items-center">
+                    <svg
+                        class="mr-3 -ml-1 size-9 animate-spin fill-third"
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                    >
+                        <circle
+                            class="opacity-25"
+                            cx="12"
+                            cy="12"
+                            r="12"
+                        ></circle>
+                        <path
+                            class="opacity-65"
+                            d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+                        ></path>
+                    </svg>
+                </section>
             </div>
         </div>
-        <Header></Header>
+        <Header />
     </div>
 </template>
