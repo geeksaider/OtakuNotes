7b5b96fd51a0599d20e3d623e7ff7c61c00a6e7b
geeksaider <nikita.bondarev.06@gmail.com>
2025-03-11 23:39:10 +0300

    add collections catalog

---
+++ src/components/SelectionCard.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+import { defineProps } from "vue";
+
+const props = defineProps<{
+    title: string;
+    imageUrl: string;
+    route: string;
+    titleCount: number;
+}>();
+</script>
+
+<template>
+    <section
+        class="relative bg-white ring ring-first aspect-square w-72 justify-center flex rounded-lg cursor-pointer overflow-hidden transition-transform transform hover:scale-105 hover:shadow-lg"
+    >
+        <img
+            :src="imageUrl"
+            alt="Anime Collection"
+            class="object-cover w-full h-full"
+        />
+        <div
+            class="absolute inset-0 bg-black bg-opacity-50 flex flex-col items-center justify-center"
+        >
+            <h3 class="text-2xl font-extrabold text-white mb-2 text-center">
+                {{ title }}
+            </h3>
+            <p class="text-lg text-white">{{ titleCount }} тайтлов</p>
+        </div>
+    </section>
+</template>
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -12,6 +12,7 @@ import Pagination from "@/components/Pagination.vue";
 import FilterList from "@/components/FilterList.vue";
 import FilterButton from "@/components/FilterButton.vue";
 import ContentTemplate from "@/components/ContentTemplate.vue";
+import Loading from "@/components/Loading.vue";
 
 const { showFilters, param, safetySearch, apply } = useFilters();
 const { animeList, loading, query, lastPage, fetchData } = useAnimeList(
@@ -86,23 +87,7 @@ const { gridColsNum } = useGrid();
                     v-if="loading && query != ''"
                     class="flex justify-center min-h-screen"
                 >
-                    <svg
-                        class="mr-3 -ml-1 size-9 animate-spin fill-third"
-                        xmlns="http://www.w3.org/2000/svg"
-                        fill="none"
-                        viewBox="0 0 24 24"
-                    >
-                        <circle
-                            class="opacity-25"
-                            cx="12"
-                            cy="12"
-                            r="12"
-                        ></circle>
-                        <path
-                            class="opacity-45"
-                            d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
-                        ></path>
-                    </svg>
+                    <Loading></Loading>
                 </section>
 
                 <Pagination
