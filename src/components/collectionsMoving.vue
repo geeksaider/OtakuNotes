06fe1e09faa28c89dd648ad6d3773fd9db4a26fb
geeksaider <132765681+geeksaider@users.noreply.github.com>
2025-03-12 07:00:19 +0000

    temp commit

---
+++ src/components/collectionsMoving.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+import { RouterLink, useRoute } from "vue-router";
+import { computed } from "vue";
+
+interface Props {
+  link: string;
+}
+
+const { link } = defineProps<Props>();
+
+const route = useRoute();
+const isActive = computed(() => route.path === link);
+</script>
+
+<template>
+  <RouterLink
+    class="text-2xl relative group"
+    :class="{
+      'font-bold': isActive,
+    }"
+    :to="link"
+  >
+    <slot></slot>
+    <div
+      class="w-0 h-0.5 transition-all mx-auto bg-third rounded-3xl group-hover:w-[90%]"
+    />
+  </RouterLink>
+</template>
