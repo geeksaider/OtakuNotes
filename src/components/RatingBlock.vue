ef474e249a10b275ce1a72c5f91e160761f818d3
geeksaider <132765681+geeksaider@users.noreply.github.com>
2025-05-31 23:46:45 +0300

    design edit

--- src/components/RatingBlock.vue
+++ src/components/RatingBlock.vue
@@ -2,7 +2,7 @@
 
 <template>
   <div
-    class="flex flex-col justify-center gap-2 items-center p-4 bg-background text-center rounded-lg border border-primary-500/10"
+    class="flex flex-col relative justify-center gap-2 items-center p-4 bg-background text-center rounded-lg border border-primary-500/10"
   >
     <div class="w-full bg-transparent text-sm text-black/60 mb-1">
       <slot name="header" />
--- src/components/WatchingState.vue
+++ src/components/WatchingState.vue
@@ -2,6 +2,7 @@
 import { ref, onMounted, watch } from "vue";
 import { useApi } from "@/composables/useAPI";
 import RatingBlock from "@/components/RatingBlock.vue";
+import PrimaryButton from "@/components/PrimaryButton.vue";
 
 interface Props {
   id: string;
@@ -42,49 +43,54 @@ const loadUserData = async () => {
 </script>
 
 <template>
-  <div class="grid grid-cols-3 gap-4">
-    <RatingBlock>
-      <template v-slot:header>Status</template>
-      <select
-        v-model="status"
-        class="bg-transparent text-center text-primary-500 text-xl"
-      >
-        <option value="0" disabled>—</option>
-        <option value="watching">Смотрю</option>
-        <option value="watched">Завершено</option>
-        <option value="dropped">Брошено</option>
-        <option value="planning">В планах</option>
-      </select>
-    </RatingBlock>
+  <section class="flex flex-col gap-4">
+    <div class="grid grid-cols-3 gap-4">
+      <RatingBlock>
+        <template v-slot:header> 📺 Status</template>
+        <select
+          v-model="status"
+          class="bg-transparent text-center text-primary-500 text-xl"
+        >
+          <option value="0" disabled>—</option>
+          <option value="watching">Смотрю</option>
+          <option value="watched">Завершено</option>
+          <option value="dropped">Брошено</option>
+          <option value="planning">В планах</option>
+        </select>
+      </RatingBlock>
 
-    <RatingBlock>
-      <template v-slot:header>⭐ Оценка</template>
-      <input
-        v-model="rating"
-        type="number"
-        min="0"
-        max="10"
-        step="0.5"
-        class="bg-transparent font-logo appearance-none border-0 outline-none shadow-none hover:border-b active:border-b text-center text-primary-500 text-xl"
-        placeholder="-"
-      />
-    </RatingBlock>
-
-    <RatingBlock>
-      <template v-slot:header>🎯 Прогресс</template>
-      <div class="flex items-center">
+      <RatingBlock>
+        <template v-slot:header>⭐ U Score</template>
         <input
-          v-model="episodesWatched"
+          v-model="rating"
           type="number"
           min="0"
-          :max="totalEpisodes"
-          class="bg-transparent text-right pr-0.5 font-logo text-primary-500 text-xl"
-          :style="`width:${String(totalEpisodes).length + 0.5}em`"
+          max="10"
+          step="0.5"
+          class="bg-transparent font-logo appearance-none border-0 outline-none shadow-none hover:border-b active:border-b text-center text-primary-500 text-xl"
+          placeholder="-"
         />
-        <span class="text-sm text-black/40 -ml-1"
-          >/ {{ totalEpisodes || "?" }}</span
-        >
-      </div>
-    </RatingBlock>
-  </div>
+      </RatingBlock>
+
+      <RatingBlock>
+        <template v-slot:header>🎯 Progress</template>
+        <div class="flex items-center">
+          <input
+            v-model="episodesWatched"
+            type="number"
+            min="0"
+            :max="totalEpisodes"
+            class="bg-transparent text-right pr-0.5 font-logo text-primary-500 text-xl"
+            :style="`width:${String(totalEpisodes).length + 0.5}em`"
+          />
+          <span class="text-sm text-black/40 -ml-1"
+            >/ {{ totalEpisodes || "?" }}</span
+          >
+        </div>
+      </RatingBlock>
+    </div>
+    <PrimaryButton :disabled="true" @click="">
+      <span class="text-primary-500">Update</span>
+    </PrimaryButton>
+  </section>
 </template>
